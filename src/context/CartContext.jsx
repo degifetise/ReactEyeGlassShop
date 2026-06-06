bac1492139e64bdc33f90ac8eed4860fa3ac0a4a
degife tise <degifetise09@gmail.com>
2026-06-06 11:59:42 +0300

    Added global cart context to handle cart logic

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,15 +1,72 @@
-import { createContext, useState } from "react";
-
-
+import { createContext, useContext, useEffect, useState } from "react";
 
 const CartContext = createContext();
 
-export function CartProvider({children}) {
+export function CartProvider({ children }) {
   const [cart, setCart] = useState(() => {
-    const saved = localStorage.getItem('eye-wear_cart');
+    const saved = localStorage.getItem("eye-wear_cart");
     return saved ? JSON.parse(saved) : [];
   });
 
+  useEffect(() => {
+    localStorage.setItem("eye-wear_cart", JSON.stringify(cart));
+  }, [cart]);
 
+  const addToCart = (product) => {
+    setCart((prev) => {
+      const existing = prev.find((item) => item.id === product.id);
 
+      if (existing) {
+        return prev.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item,
+        );
+      }
+      return [...prev, { ...product, quantity: 1 }];
+    });
+  };
+
+  const updateQuantity = (id, amount) => {
+    setCart((prev) => {
+      prev.map((item) =>
+        item.id === id
+          ? {
+              ...item,
+              quantity: item.quantity + amount,
+            }
+          : item,
+      );
+    }).filter((item) => item.quantity > 0);
+  };
+  const removeFromCart = (id) => {
+    setCart((prev) => prev.filter((item) => item.id !== id));
+  };
+
+  const clearCart = () => setCart([]);
+
+  const totalItems = cart.reduce((sum, item) => sum + item.quantity, 0);
+  const totalPrice = cart.reduce(
+    (sum, item) => sum + item.price * item.quantity,
+    0,
+  );
+
+  return (
+    <CartContext.Provider
+      value={{
+        cart,
+        addToCart,
+        updateQuantity,
+        removeFromCart,
+        clearCart,
+        totalPrice,
+        totalItems,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
 }
+
+
+export const useCart = () => useContext(CartContext);
